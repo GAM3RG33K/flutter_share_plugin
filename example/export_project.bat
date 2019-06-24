7daf6cd031e575a63bed34ea7d28f18df2b99006
Harshvardhan Joshi <hj2931996@gmail.com>
2019-06-24 11:28:44 +0530

    add a script to clean iOS build's left over

--- example/export_project.bat
+++ example/export_project.bat
@@ -1,6 +1,7 @@
+@echo off
 set currentDir=%cd%
 for %%d in ("%currentDir%") do set "dirName=%%~nxd"
 echo %currentDir%
 echo %dirName%
-flutter clean && 7z a -tzip %dirName%.zip .
+flutter clean && call clean_leftovers.bat && 7z a -tzip %dirName%.zip .
 pause
