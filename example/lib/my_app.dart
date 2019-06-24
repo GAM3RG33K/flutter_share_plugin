9005d1231bff489eed41f316cb04eb420b09e85a
Harshvardhan Joshi <hj2931996@gmail.com>
2019-06-24 16:50:16 +0530

    changes:  - add new customized method for sharing only text, only files or both  - update example project for demostrating all the new methods

--- example/lib/my_app.dart
+++ example/lib/my_app.dart
@@ -1,70 +1,146 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_share_plugin/flutter_share_plugin.dart';
+import 'package:file_picker/file_picker.dart';
 
-import 'utils/general_utils.dart';
+import 'utils/notification_helper.dart';
+import 'utils/permission_manager.dart';
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  static GlobalKey widgetKey = GlobalKey();
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterShare.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return RepaintBoundary(
-      key: widgetKey,
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Plugin example app'),
-          ),
-          body: Center(
-            child: Column(
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String selectedFileName = 'None';
+  String selectedFilePath = 'Unknown';
+  final textController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter file share app demo'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("file: $selectedFileName"),
+            Text("path: $selectedFilePath"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('Running on: $_platformVersion\n'),
                 RaisedButton(
-                  child: Text("Share"),
-                  onPressed: () async {
-                    await GeneralUtils.takeScreenshot(widgetKey);
-                  },
+                  onPressed: () => showChooser(),
+                  child: Text('Choose file'),
                 ),
+                Container(
+                  padding: EdgeInsets.all(5.0),
+                ),
+                RaisedButton(
+                  onPressed: () =>
+                      shareFile(selectedFileName, selectedFilePath),
+                  child: Text('Share file'),
+                )
               ],
             ),
-          ),
+            RaisedButton(
+              onPressed: () => chooseAndShare(),
+              child: Text('Choose & Share file'),
+            ),
+            TextField(autofocus: true, controller: textController),
+            RaisedButton(
+              onPressed: () => shareText(textController.text),
+              child: Text('Share text'),
+            )
+          ],
         ),
       ),
     );
   }
+
+  shareFile(String fileName, String filePath) {
+    getFileBytes(filePath).then((bytes) async {
+      var text = "sharing file: $fileName";
+      NotificationHelper.showToast(text, false);
+      await FlutterShare.shareFileWithText(
+          textContent: text, filePath: filePath);
+    });
+  }
+
+  showChooser() async {
+    var task = () {
+      getFileFromChooser().then((filePath) {
+        setState(() {
+          this.selectedFileName = getFileNameFromPath(filePath);
+          this.selectedFilePath = filePath;
+        });
+      });
+    };
+    PermissionManager.performTaskWithPermission(AppPermission.Storage, task);
+  }
+
+  shareText(String text) {
+    var textContent = "sharing text: $text";
+
+    FlutterShare.shareText(text).then((text) {
+      //DO something here if needed.
+      NotificationHelper.showToast(textContent, false);
+    });
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    textController.dispose();
+    super.dispose();
+  }
+
+  chooseAndShare() {
+    var task = () {
+      getFileFromChooser().then((filePath) {
+        setState(() {
+          this.selectedFileName = getFileNameFromPath(filePath);
+          this.selectedFilePath = filePath;
+        });
+        FlutterShare.shareFile(filePath);
+      });
+    };
+    PermissionManager.performTaskWithPermission(AppPermission.Storage, task);
+  }
+}
+
+Future<String> getFileFromChooser() async {
+  // Pick a single file directly
+  String filePath = await FilePicker.getFilePath(
+      type: FileType
+          .ANY); // will return a File object directly from the selected file
+  return filePath;
+}
+
+Future<List<int>> getFileBytes(String filePath) async {
+  var file = File.fromUri(Uri.parse(filePath));
+  var fileBytes = await file.readAsBytes();
+  return fileBytes;
+}
+
+String getFileNameFromPath(String filePath) {
+  int fileNameStartIndex = filePath.lastIndexOf("/") + 1;
+  return filePath.substring(fileNameStartIndex, filePath.length);
 }
--- lib/flutter_share_plugin.dart
+++ lib/flutter_share_plugin.dart
@@ -4,6 +4,10 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
+/// This is the entry class for the plugin.
+///
+/// This is a utility class, so all the methods here are static only.
+/// All the dependencies are passed as the parameters in the.
 class FlutterShare {
   static const MethodChannel _channel =
       const MethodChannel('flutter_share_plugin');
@@ -13,29 +17,100 @@ class FlutterShare {
     return version;
   }
 
-  /// This method will call the respective OS's method implementation throigh method channel
+  /// This method will call the respective OS's method implementation through method channel
   /// provided by flutter
   ///
+  /// There are all the parameters are kept optional right now, to provide customizaed method
+  /// implementations.
+  ///
+  /// For Sharing text or link [textContent] is content is used, while for sharing a file [fileName] and [bytes]
+  /// are used.
+  ///
+  /// Any of the above parameter can be null at any time.
   /// NOTE: this plugin can only provide functionality upto showing share UI
   /// Anything after Showing the UI is upto the respective OS to handle.
   static Future<bool> share(
-      {String textContent, String fileName, List<int> bytes}) async {
+      {String textContent = "Remove or Replace with your text",
+      String fileName,
+      List<int> bytes}) async {
+    if (fileName == null) {
+      fileName = "";
+    }
     if (textContent.isEmpty && fileName.isEmpty) {
       print(
-          "FlutterShare: share(textContent, fileUrl) method requires at least one Non-empty parameter.");
+          "FlutterShare: share(textContent, fileName, bytes) method requires at least one Non-empty parameter.");
       return false;
     }
 
-    if (fileName.isNotEmpty) {
+    if (fileName.isNotEmpty && bytes != null && bytes.length > 0) {
       final tempDir = await getTemporaryDirectory();
       final file = await new File('${tempDir.path}/$fileName').create();
       await file.writeAsBytes(bytes);
-      print("file size: ${ await file.length()} bytes");
+      print("file size: ${await file.length()} bytes");
     }
     final bool success = await _channel.invokeMethod('share', <String, dynamic>{
       'content': textContent,
       'fileName': fileName,
     });
     return success;
+  }
+
+  /// Method implementation for sharing file with a [textContent], file is accessed via [filePath] given as
+  /// parameter
+  ///
+  ///Under the hood this method uses
+  ///```dart share(textContent, fileName, bytes); ```
+  static Future<bool> shareFileWithText(
+      {String textContent, String filePath = ""}) async {
+    if (textContent == null) {
+      textContent = "";
+    }
+
+    String fileName;
+    var bytes;
+    if (filePath != null && filePath.isNotEmpty) {
+      fileName = _getFileNameFromPath(filePath);
+      bytes = await _getFileBytes(filePath);
+    }
+
+    return share(textContent: textContent, fileName: fileName, bytes: bytes);
+  }
+
+  ///Method implementation for sharing only text,
+  ///
+  ///Under the hood this method uses
+  ///```dart shareFileWithText(textContent, filePath); ```
+  ///
+  static Future<bool> shareText(String textContent) async {
+    return shareFileWithText(textContent: textContent, filePath: null);
+  }
+
+  ///Method implementation for sharing only file,
+  ///
+  ///
+  ///
+  ///Under the hood this method uses
+  ///```dart shareFileWithText(textContent, filePath); ```
+  ///
+  static Future<bool> shareFile(String filePath) async {
+    return shareFileWithText(textContent: null, filePath: filePath);
+  }
+
+  /// This method will get a file using the given [filePaht].
+  /// After fetching the files,
+  ///   then file will be converted to list of byte
+  ///
+  /// list of bytes is returnes.
+  static Future<List<int>> _getFileBytes(String filePath) async {
+    var file = File.fromUri(Uri.parse(filePath));
+    var fileBytes = await file.readAsBytes();
+    return fileBytes;
+  }
+
+  /// This method will get fileName from the given [filePaht].
+  /// name of the file is returned.
+  static String _getFileNameFromPath(String filePath) {
+    int fileNameStartIndex = filePath.lastIndexOf("/") + 1;
+    return filePath.substring(fileNameStartIndex, filePath.length);
   }
 }
