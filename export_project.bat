4b94e6e2eadfd7bb8225271bc8395c73cf723720
Harshvardhan Joshi <hj2931996@gmail.com>
2019-06-25 08:30:09 +0530

    removing script files form repo

--- export_project.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set currentDir=%cd%
-for %%d in ("%currentDir%") do set "dirName=%%~nxd"
-echo %currentDir%
-echo %dirName%
-flutter clean && call clean_leftovers.bat && 7z a -tzip %dirName%.zip .
-pause
