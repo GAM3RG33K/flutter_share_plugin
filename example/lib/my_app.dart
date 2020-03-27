1e364ae37056f0d40d163a85152b11c3a1a6b7a9
Harshvardhan Joshi <hj2931996@gmail.com>
2020-03-27 07:10:25 +0530

    changes:  - code changes according to the pana analysis 2

--- example/lib/my_app.dart
+++ example/lib/my_app.dart
@@ -132,9 +132,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
 Future<String> getFileFromChooser() async {
   // Pick a single file directly
-  String filePath = await FilePicker.getFilePath(
-      type: FileType
-          .ANY); // will return a File object directly from the selected file
+  String filePath = await FilePicker.getFilePath(type: FileType
+      .any); // will return a File object directly from the selected file
   return filePath;
 }
 
